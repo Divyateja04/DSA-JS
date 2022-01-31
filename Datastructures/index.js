b514631d0c820b24780092fe114f9bc2863f5b0f
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-01-31 23:54:46 +0530

    Changed spellings

--- Datastructures/index.js
+++ Datastructures/index.js
@@ -1,5 +1,5 @@
 //Datastructures is a collection of values basically a storage place for your files but each specializes in it's own thing and based on the scenario we choose the datastructures
 //Generally variables are stored in RAM while storage is used for storing actual files
 //A 8 bit can store 2^8-1=255, 16bit can store 2^16-1
-//A 32 bit and 64 bit store 2^32-1 and 2^64-1 respectively and similarly each datastructure has similar limits
+//A 32 bit and 64 bit store 2^32-1 and 2^64-1 respectively and similarly each datastructures has similar limits
 
